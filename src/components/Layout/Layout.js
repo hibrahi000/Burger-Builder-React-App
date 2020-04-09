885639202a73dff2cdeb1499c9909e4548dd02e5
Hashmat Ibrahimi <Hashmatibrahimi0711@gmail.com>
2020-04-09 15:55:56 -0400

    resuming error managment code status

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import classes from './Layout.module.css';
+
 const Layout = (props) => (
 	<Aux>
-		{/* <div>Toolbar, SideDrawer</div> */}
 		<Toolbar />
+		<SideDrawer />
 		<main className={classes.Content}>{props.children}</main>
 	</Aux>
 );
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -4,15 +4,15 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './Toolbar.module.css';
 
 const toolbar = (props) => (
-  <header className={classes.Toolbar}>
-    <div>MENU</div>
-
-    <Logo />
-
-    <nav>
-      <NavigationItems />
-    </nav>
-  </header>
+	<header className={classes.Toolbar}>
+		<div>MENU</div>
+		<div className={classes.LogoContainer}>
+			<Logo />
+		</div>
+		<nav>
+			<NavigationItems />
+		</nav>
+	</header>
 );
 
 export default toolbar;
