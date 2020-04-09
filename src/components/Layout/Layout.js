7b0040278ef97c36840feb498c2dae8b8e5021e6
Hashmat Ibrahimi <Hashmatibrahimi0711@gmail.com>
2020-04-09 18:45:35 -0400

    completed outlook of burger app

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,36 @@
-import React from 'react';
-import Aux from '../../hoc/Aux';
-import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
-import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
-import classes from './Layout.module.css';
+import React, { Component } from "react";
+import Aux from "../../hoc/Aux";
+import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
+import classes from "./Layout.module.css";
 
-const Layout = (props) => (
-	<Aux>
-		<Toolbar />
-		<SideDrawer />
-		<main className={classes.Content}>{props.children}</main>
-	</Aux>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  };
+
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+
+  sideDrawerToggleHandler = () => {
+    this.setState(prevState => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
+  };
+
+  render() {
+    return (
+      <Aux>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <SideDrawer
+          closed={this.sideDrawerClosedHandler}
+          open={this.state.showSideDrawer}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
 
 export default Layout;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import Logo from '../../Logo/Logo';
-import NavigationItems from '../NavigationItems/NavigationItems';
-import classes from './Toolbar.module.css';
+import React from "react";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
+import classes from "./Toolbar.module.css";
 
-const toolbar = (props) => (
-	<header className={classes.Toolbar}>
-		<div>MENU</div>
-		<div className={classes.LogoContainer}>
-			<Logo />
-		</div>
-		<nav>
-			<NavigationItems />
-		</nav>
-	</header>
+const toolbar = props => (
+  <header className={classes.Toolbar}>
+    <DrawerToggle clicked={props.drawerToggleClicked} />
+    <div className={classes.LogoContainer}>
+      <Logo />
+    </div>
+    <nav className={classes.DesktopOnly}>
+      <NavigationItems />
+    </nav>
+  </header>
 );
 
 export default toolbar;
